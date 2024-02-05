466e6df08cf14e3427789a40fe8de3211b9612d7
Dinesh yadav <dineshyadav74950@gmail.com>
2024-02-05 19:16:31 +0530

    Pipe | Parameterised Pipes in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,31 @@ export class AppComponent {
   title = 'TwoWayDataBindingInAngular16';
   //data : string="Dineshydauvnshee";
   data: string | null = "Dineshydauvnshee";
+  employees : any[]=[
+    {
+      empId:101,
+      name: 'Dineshydauvnshee',
+      address:'allhabad'
+    },{
+      empId:102,
+      name:'Pawan',
+      address:'varansi'
+    },{
+      empId:103,
+      name:'Devesh',
+      address:'aayodhya'
+    },{
+      empId:104,
+      name:'sanntosh',
+      address:'madhubani'
+    },{
+      empId:105,
+      name:'Ravi',
+      address:'jaunpur'
+    }
+  ]
+  dob = new Date(5-2-2024);
+  salary:number = 5000000;
+  
 }
+
